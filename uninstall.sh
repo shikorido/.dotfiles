7c39744c3c0565b61313f06f1585a1f50ea1a20e
shikorido <bliznetsk2008@gmail.com>
2025-05-23 18:54:11 +0300

    Initial commit

---
+++ uninstall.sh
@@ -0,0 +1,36 @@
+# DOTFILES management
+# kali
+# uninstall.sh
+
+# Should be ran from the worktree root but just in case
+script_dir=`readlink -f "$0"`
+script_dir=${script_dir%/*}
+! [ "$PWD" = "$script_dir" ] && __PWD_OLD=$PWD && cd "$script_dir"
+
+if ! [ -s .master_root ]; then
+    echo "kali/uninstall.sh: $script_dir/.master_root is empty or does not exist! It should point to the root of master branch in order to source utils.sh!"
+    exit 1
+fi
+master_root=`cat .master_root`
+
+# Setting DOTFILES for every branch individually (cause of splitted configs)
+export DOTFILES=$script_dir
+. "$master_root/utils/utils.sh"
+
+setup_variables
+check_dotfiles
+prepare_stow_packages
+check_conflicts
+perform_unstow
+
+# Propagate uninstallation
+[ "$WSL" = 1 ] && {
+    cd wsl
+    [ -f uninstall.sh ] && ./uninstall.sh
+    cd "$script_dir"
+}
+
+[ -n "$__PWD_OLD" ] && cd "$__PWD_OLD" && unset __PWD_OLD
+
+exit 0
+
